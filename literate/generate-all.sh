1869532aa338fc5acab718f7396bcba042ff0b1b
Rich Cheng <sedm0784@users.noreply.github.com>
2024-12-08 00:34:51 +0000

    Add literate navbar and home page. gvimrc

--- literate/generate-all.sh
+++ literate/generate-all.sh
@@ -10,6 +10,9 @@ fi
 
 # Tangle
 "$inweb" -read-language literate/Vimscript.ildf literate/vimrc -tangle-to vimfiles/vimrc
+"$inweb" -read-language literate/Vimscript.ildf literate/gvimrc.inweb -tangle-to vimfiles/gvimrc
 
 # Weave
+"$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member overview -weave
 "$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member vimrc -weave
+"$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member gvimrc -weave
