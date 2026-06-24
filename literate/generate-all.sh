0663497a8f742b49dfdad74126e23eab9c579db8
sedm0784 <sedm0784@users.noreply.github.com>
2026-06-24 14:58:52 +0100

    Add python.vim after/ftplugin web
    Merge python-specific config into literate version.

--- literate/generate-all.sh
+++ literate/generate-all.sh
@@ -11,11 +11,14 @@ fi
 # Tangle
 "$inweb" -read-language literate/Vimscript.ildf literate/vimrc -tangle-to vimfiles/vimrc
 "$inweb" -read-language literate/Vimscript.ildf literate/gvimrc.inweb -tangle-to vimfiles/gvimrc
+"$inweb" -read-language literate/Vimscript.ildf literate/after/ftplugin/python.inweb -tangle-to vimfiles/after/ftplugin/python.vim
 
 # Weave
 # FIXME: These paths are currently specified in the colony file
 mkdir -p literate/html/vimrc
 mkdir -p literate/html/gvimrc
+mkdir -p literate/html/after/ftplugin/python.vim
 "$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member overview -weave
 "$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member vimrc -weave
 "$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member gvimrc -weave
+"$inweb" -read-language literate/Vimscript.ildf -colony literate/colony.txt -member python.vim -weave
